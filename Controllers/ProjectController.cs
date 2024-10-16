6d3b227c785401d8863f3742755d29e44178194e
Ahmed Hazim <1ahmadhazem@gmail.com>
2024-10-16 04:08:58 +0300

    Creating a Project model and a Project Controller.

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,52 @@
+﻿using AonFreelancing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AonFreelancing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private static List<Project> ProjectList = new List<Project>();
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(ProjectList);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            ProjectList.Add(project);
+            return CreatedAtAction(nameof(Create), new { Id = project.Id }, project);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProject(int id)
+        {
+            var Project = ProjectList.FirstOrDefault(p => p.Id == id);
+
+            if (Project == null)
+            {
+                return NotFound("The resoucre is not found!");
+            }
+
+            return Ok(Project);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var Project = ProjectList.FirstOrDefault(p => p.Id == id);
+            if (Project != null)
+            {
+                ProjectList.Remove(Project);
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,25 @@
+﻿namespace AonFreelancing.Models
+{
+    public class Project
+    {
+        private static int _projectCount = 0;
+        private int _id = _projectCount;
+        public string Title { get; set; }
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+        public Project()
+        {
+            _projectCount++;
+        }
+        public Project(string titel)
+        {
+            _projectCount++;
+            Title = titel;
+        }
+    }
+}
