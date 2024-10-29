62c91ad0b5a165a02ce908438f3ebb504ec0a9cc
Ahmed Hazim <1ahmadhazem@gmail.com>
2024-10-29 17:02:45 +0300

    Create: Creating and implementing FreelancerInputDTO class.

---
+++ Models/DTOs/FreelancerInputDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AonFreelancing.Models.DTOs
+{
+    public class FreelancerInputDTO : UserDTO
+    {
+        [DisallowNull]
+        [Required]
+        [MinLength(4, ErrorMessage = "Skills length is too short.")]
+        public string Skills { get; set; }
+    }
+}
