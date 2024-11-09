f16de3f4ad71d515051f53da27186d2602a7d213
fadiramzi <fadiramzi.99@gmail.com>
2024-11-10 00:11:50 +0300

    week 06 done

--- Controllers/Mobile/v1/ProjectsController.cs
+++ Controllers/Mobile/v1/ProjectsController.cs
@@ -3,8 +3,10 @@ using AonFreelancing.Models;
 using AonFreelancing.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace AonFreelancing.Controllers.Mobile.v1
 {
@@ -13,24 +15,39 @@ namespace AonFreelancing.Controllers.Mobile.v1
     [ApiController]
     public class ProjectsController : BaseController
     {
-        //private readonly MainAppContext _mainAppContext;
-        //public ProjectsController(MainAppContext mainAppContext)
-        //{
-        //    _mainAppContext = mainAppContext;
-        //}
+        private readonly MainAppContext _mainAppContext;
+        private readonly UserManager<User> _userManager;
+        public ProjectsController(
+            MainAppContext mainAppContext,
+            UserManager<User> userManager
+            )
+        {
+            _mainAppContext = mainAppContext;
+            _userManager = userManager;
+        }
 
-        //[HttpPost]
-        //public IActionResult CreateProject([FromBody] ProjectInputDTO project)
-        //{
-        //    Project p = new Project();
-        //    p.Title = project.Title;
-        //    p.Description = project.Description;
-        //    p.ClientId = project.ClientId;
 
-        //    _mainAppContext.Projects.Add(p);
-        //    _mainAppContext.SaveChanges();
-        //    return Ok(CreateSuccessResponse(p));
-        //}
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateProject([FromBody] ProjectInputDTO project)
+        {
+            var Username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var UserClient = await _userManager.FindByNameAsync(Username);
+
+            Project p = new Project();
+            p.Title = project.Title;
+            p.Description = project.Description;
+            p.ClientId = UserClient.Id;
+            p.QualificationName = project.QualificationName;
+            p.Budget = project.Budget;
+            p.PriceType = project.PriceType;
+            p.Duration = project.Duration;
+
+            _mainAppContext.Projects.Add(p);
+            _mainAppContext.SaveChanges();
+            return Ok(CreateSuccessResponse(p));
+        }
 
 
         //[HttpGet("{id}")]
--- Models/DTOs/ProjectInputDTO.cs
+++ Models/DTOs/ProjectInputDTO.cs
@@ -1,14 +1,30 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace AonFreelancing.Models.DTOs
 {
     public class ProjectInputDTO
     {
+        [Required]
+        [MaxLength(512, ErrorMessage ="Length too long")]
         public string Title { get; set; }
 
         [AllowNull]
         public string Description { get; set; }
 
-        public int ClientId { get; set; }//FK
+        [Required]
+        public string QualificationName { get; set; }
+
+        [Required]
+        public int Duration { get; set; }//Number of days
+
+        [Required]
+        [AllowedValues("PerHour","Fixed")]
+        public string PriceType { get; set; }
+
+        [Required]
+        public decimal Budget { get; set; }
+
+
     }
 }
