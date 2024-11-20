852eb35342f4083641b2ff030140f94aeb6b2b80
KAJOiq <abdalrahmanahmed298@gmail.com>
2024-11-21 01:32:00 +0300

    Revert "شكني ومدايتحول"
    This reverts commit 6ce723b5bf608d4ab7944857f844e6b0a6fd68be.

--- Models/Otp.cs
+++ Models/Otp.cs
@@ -10,10 +10,10 @@ namespace AonFreelancing.Models
         [Key]
         public string PhoneNumber { get; set; }
 
-        public string? Code { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? ExpiresAt { get; set; }
-        public bool? IsUsed { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public bool IsUsed { get; set; }
 
     }
 }
